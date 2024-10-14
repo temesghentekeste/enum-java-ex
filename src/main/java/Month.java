01de68af1971544767198a12dfbb480fb338c0af
temesghentekeste <mail.temesghen.tekeste@gmail.com>
2024-10-15 01:04:47 +0300

    Move file out of develhope package

---
+++ src/main/java/Month.java
@@ -0,0 +1,40 @@
+public enum Month {
+    JANUARY("January"),
+    FEBRUARY("February"),
+    MARCH("March" ),
+    APRIL("April"),
+    MAY("April"),
+    JUNE("June"),
+    JULY("July"),
+    AUGUST("August"),
+    SEPTEMBER("September"),
+    OCTOBER("October"),
+    NOVEMBER("November"),
+    DECEMBER("December");
+
+    private String name;
+
+    Month(String name) {
+        this.name = name;
+    }
+
+    public String info() {
+        if(name.endsWith("y")) {
+            return "ends with y";
+        }
+        return "doesn't end with y";
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + info();
+    }
+}
+
+class MonthDemo {
+    public static void main(String[] args) {
+        for (Month value : Month.values()) {
+            System.out.println(value);
+        }
+    }
+}
